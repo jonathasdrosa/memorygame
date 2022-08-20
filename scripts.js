eeeefd0068bd251982d6ae8efca4edb2e1427c61
jonathasdrosa <111540871+jonathasdrosa@users.noreply.github.com>
2022-08-20 01:46:30 -0300

    finalizando mas não esta rodando

--- scripts.js
+++ scripts.js
@@ -1,14 +1,63 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let lockboard = false;
 let firstCard, secondCard;
 
 function flipCard() {
+    if (lockboard) return;
+    if (this ===firstCard) return;
+
     this.classList.add('flip');
+
     if (!hasFlippedCard) {
         hasFlippedCard = true;
         firstCard = this;
+        return;
     }
+
+    secondCard = this;
+
+    checkForMatch();
+
 }
+
+function checkForMatch() {
+   let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+   isMatch ? disableCards() : unflipCards();
+
+}
+
+function disableCards(){
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetboard();
+}
+
+function unflipCards() {
+        lockboard = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+       resetBoard();
+    }, 1500);
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockboard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    });
+})();
+
+
+
 
 cards.forEach(card => card.addEventListener('click', flipCard));
